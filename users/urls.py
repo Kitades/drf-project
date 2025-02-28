c4cc42a08edce4cdf79ee24faca6ddceff8ec2e6
Maksim <maks.severgin80@gmail.com>
2025-02-28 20:48:19 +0700

    root-drf1

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,13 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView
+
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import filters
+from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import Payments
-from users.serializers import PaymentsSerializer
+from users.models import Payments, User
+from users.serializers import PaymentsSerializer, UserSerializer
 
 
 class PaymentsViewSet(ModelViewSet):
@@ -11,6 +12,16 @@ class PaymentsViewSet(ModelViewSet):
     filter_backends = [filters.OrderingFilter]
     filterset_fields = ('payment_type','payment_course', 'payment_lesson')
     ordering_fields = ('date_payment',)
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 
